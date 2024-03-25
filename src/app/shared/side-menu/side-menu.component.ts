240868ecfe112dc54de713918607a78d0cf36d94
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-25 16:51:24 +0100

    alias

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css',
 })
