4ab95d4b80346090423bfaf8e6fc4e5ff5a74fc8
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-26 09:35:48 +0100

    change detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush, //! Angular no está pendiente de todos los cambios que ocurren, solo las señales envían el cambio. Mejora la performance
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
   styleUrl: './change-detection.component.css',
 })
-export class ChangeDetectionComponent {}
+export class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update((value) => ({
+        ...value,
+        name: 'React',
+      })); //! Debe retornar otro objeto diferente para que se detecte el cambio
+      console.log('Hecho');
+    }, 3000);
+  }
+}
