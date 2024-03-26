13d23e8d3fd7a63f2bb9d1f5701d0b679a56c540
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-26 14:15:20 +0100

    user

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
 
+import { TitleComponent } from '@shared/title/title.component';
+import { switchMap } from 'rxjs';
+import { UsersService } from '../../../services/user.service';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
 })
 export class UserComponent {
+  private route = inject(ActivatedRoute);
+  private usersService = inject(UsersService);
 
+  public user = toSignal(
+    //! toSignal convierte un observable en un signal
+    this.route.params.pipe(
+      switchMap(({ id }) => this.usersService.getUserById(id))
+    )
+  );
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `Información del usuario: ${this.user()?.first_name} ${
+        this.user()?.last_name
+      } `;
+    }
+
+    return 'Información del usuario';
+  });
 }
