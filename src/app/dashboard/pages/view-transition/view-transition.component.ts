8ee9d54ac58aa69ce8eea7fd9f08fd1f73dbc6ec
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-26 10:42:28 +0100

    wip

--- src/app/dashboard/pages/view-transition/view-transition.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition.component.ts
@@ -1,9 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { TitleComponent } from '@shared/title/title.component';
 @Component({
   selector: 'app-view-transition',
   standalone: true,
-  imports: [],
-  template: ` <div class="view-transition"></div> `,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="View Transition 2" />
+
+    <section class="flex justify-end">
+      <img
+        srcset="
+          https://images.hola.com/imagenes/mascotas/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg?tx=w_680
+        "
+        alt="Picsum"
+        width="200"
+        height="300"
+        style="view-transition-name: hero1"
+      />
+
+      <div
+        class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded"
+        style="view-transition-name: hero2"
+      ></div>
+    </section>
+  `,
 })
 export class ViewTransitionComponent {}
